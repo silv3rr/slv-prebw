53ed7d883b72383044cb48dcee8c13f292c8f656
slv <r3vl1s@gmail.com>
2016-11-05 17:50:44 +0100

    0.54

--- PreBW.tcl
+++ PreBW.tcl
@@ -12,7 +12,10 @@ namespace eval ::ngBot::plugin::PreBW {
     ##
     set slvprebw "$::ngBot::glroot/bin/slv-prebw.sh"
     variable scriptName ${ns}::LogEvent
-    bind evnt -|- prerehash ${ns}::DeInit
+    # If you use a different PRE event/announce you can add it here (e.g. a msgreplace)
+    # e.g. [list "PRE" "PREMP3"]
+    variable events [list "PRE"]
+    #bind evnt -|- prerehash ${ns}::DeInit
 
     if {[string equal "" $np]} {
             bind evnt -|- prerehash ${ns}::deinit
@@ -20,30 +23,35 @@ namespace eval ::ngBot::plugin::PreBW {
 
     proc init {} {
         variable np
+        variable events
         variable scriptName
         variable ${np}::precommand
-        lappend precommand(PRE) $scriptName
         putlog "\[ngBot\] PreBW :: Loaded successfully."
+        foreach event $events {
+            lappend precommand($event) $scriptName
+        }
         return
     }
 
     proc deinit {} {
         variable ns
         variable np
+        variable events
         variable scriptName
         variable ${np}::precommand
-        if {[info exists precommand(PRE)] && [set pos [lsearch -exact $precommand(PRE) $scriptName]] !=  -1} {
-            set precommand(PRE) [lreplace $precommand(PRE) $pos $pos]
+        foreach event $events {
+          if {[info exists precommand($event)] && [set pos [lsearch -exact $precommand($event) $scriptName]] !=  -1} {
+              set precommand($event) [lreplace $precommand($event) $pos $pos]
+          }
         }
         namespace delete $ns
-        catch {unbind evnt -|- prerehash ${ns}::deinit}
+        #catch {unbind evnt -|- prerehash ${ns}::deinit}
         return
     }
 
     proc LogEvent {event section logData} {
         variable slvprebw
-
-        if {![string equal "PRE" $event]} {return 1}
+        if {(![string equal "PRE" $event]) && (![string equal "PREMP3" $event])} {return 1}
         set fullpath [lindex $logData 0]
         exec $slvprebw $fullpath &
         return 1
